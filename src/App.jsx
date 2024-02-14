37c55e90dbb64f395660abd21b29b4e43cf33b0c
kelly emilly <kellyemilly178@gmail.com>
2024-02-14 11:13:58 -0300

    subindo

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer/Footer'
 import Sobre from './pages/Sobre'
 import Comentarios from './pages/Comentarios'
 import Contato from './pages/Contato'
-import Home from './pages/Home/Home'
+import Home from './pages/Home'
 
 
 
